08b351477e2e6c250adbb91f23994ec762120cfb
Noam Ben Ari <nbenari@gmail.com>
2016-07-31 17:09:56 +0300

    Replace /etc/rc.d with /etc/init.d

--- azure/redis/startup.sh
+++ azure/redis/startup.sh
@@ -5,6 +5,6 @@ echo "vm.overcommit_memory = 1" >> /etc/sysctl.conf
 echo never > /sys/kernel/mm/transparent_hugepage/enabled
 sysctl vm.overcommit_memory=1
 
-chmod +x /etc/rc.d/rc.local
-echo "echo 65535 > /proc/sys/net/core/somaxconn" >> /etc/rc.d/rc.local
-echo "echo never > /sys/kernel/mm/transparent_hugepage/enabled" >> /etc/rc.d/rc.local
+echo "echo 65535 > /proc/sys/net/core/somaxconn" >> /etc/init.d/rc.local
+echo "echo never > /sys/kernel/mm/transparent_hugepage/enabled" >> /etc/init.d/rc.local
+
