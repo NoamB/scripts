0a0dd7f3d0174a4dbb1829e9acc97380609f6635
Noam Ben Ari <nbenari@gmail.com>
2016-08-12 13:04:44 +0300

    Adding generic high performance server script from http-kit, adding some of it to redis

--- azure/server.sh
+++ azure/server.sh
@@ -6,10 +6,6 @@ echo 9999999 | sudo tee /proc/sys/fs/file-max
 echo "* - nofile 4999999" >> /etc/security/limits.conf
 
 echo 65535 > /proc/sys/net/core/somaxconn
-echo "vm.overcommit_memory = 1" >> /etc/sysctl.conf
-echo never > /sys/kernel/mm/transparent_hugepage/enabled
-sysctl vm.overcommit_memory=1
 
 echo "echo 65535 > /proc/sys/net/core/somaxconn" >> /etc/init.d/rc.local
-echo "echo never > /sys/kernel/mm/transparent_hugepage/enabled" >> /etc/init.d/rc.local
 
